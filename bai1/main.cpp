59a953cd944f2042998eb080504122615118158d
Trung Anh Nguyễn <trunganhnta.github.com>
2020-04-25 16:16:26 +0700

    bt

---
+++ bai1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class dathuc
+{
+    float *a;
+    int n;
+public:
+    dathuc()
+    {
+
+    }
+};
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
